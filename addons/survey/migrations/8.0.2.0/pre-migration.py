f7fb5ad448269d53b67d336658c3121d98b04857
sergiocorato <sergiocorato@gmail.com>
2019-11-12 19:45:52 +0100

    [IMP] upgrade models

--- addons/survey/migrations/8.0.2.0/pre-migration.py
+++ addons/survey/migrations/8.0.2.0/pre-migration.py
@@ -8,3 +8,7 @@ def migrate(cr, version):
     openupgrade.rename_models(cr, [('survey', 'survey.survey')])
     openupgrade.rename_tables(cr, [('survey_request', 'survey_user_input')])
     openupgrade.rename_models(cr, [('survey.request', 'survey.user_input')])
+    openupgrade.rename_tables(cr, [('survey_type', 'survey_type')])
+    openupgrade.rename_models(cr, [('survey.type', 'survey.type')])
+    openupgrade.rename_tables(cr, [('survey_response_line', 'survey_user_input_line')])
+    openupgrade.rename_models(cr, [('survey.response.line', 'survey.user_input_line')])
