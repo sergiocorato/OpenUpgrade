66b5765856e194ebac30dcf00e82a17af60e2bdf
sergiocorato <sergiocorato@gmail.com>
2020-07-01 07:44:29 +0200

    [UPD] asset and machinery

--- odoo/addons/openupgrade_records/lib/apriori.py
+++ odoo/addons/openupgrade_records/lib/apriori.py
@@ -36,6 +36,8 @@ renamed_modules = {
     # efatto
     'l10n_it_ddt_ext': 'l10n_it_ddt_date_start',
     'invoice_filter_country': 'account_invoice_filter_country',
+    'machine_manager_partner': 'maintenance_equipment_ownership',
+    'account_asset_management': 'assets_management',
 }
 
 merged_modules = {
@@ -57,14 +59,14 @@ merged_modules = {
     # OCA/account-analytic
     # although model is defined in "analytic", logic is in "account"
     'account_analytic_distribution': 'account',
-    'account_asset_analytic': 'account_asset_management',
+    'account_asset_analytic': 'assets_management',
     # OCA/account-financial-reporting
     'customer_activity_statement': 'partner_statement',
     'customer_outstanding_statement': 'partner_statement',
     # OCA/account-financial-tools
-    'account_asset': 'account_asset_management',
-    'account_asset_depr_line_cancel': 'account_asset_management',
-    'account_asset_disposal': 'account_asset_management',
+    'account_asset': 'assets_management',
+    'account_asset_depr_line_cancel': 'assets_management',
+    'account_asset_disposal': 'assets_management',
     'account_reversal': 'account',
     # OCA/e-commerce
     'website_sale_default_country': 'website_sale',
@@ -117,6 +119,11 @@ renamed_models = {
     'account.asset.asset': 'account.asset',
     'account.asset.depreciation.line': 'account.asset.line',
     'account.asset.category': 'account.asset.profile',
+    # Efatto #todo non serve a niente qui, va gestito nella migrazione del modulo
+    'machinery': 'maintenance.equipment',
+    'account.asset.asset': 'asset.asset',
+    'account.asset.depreciation.line': 'asset.depreciation.line',
+    'account.asset.category': 'asset.category',
 }
 
 # only used here for openupgrade_records analysis:
