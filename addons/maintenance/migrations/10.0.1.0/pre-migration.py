bb43df1c419ac3925d78edf434220ebd43e89492
sergiocorato <sergiocorato@gmail.com>
2020-07-29 12:33:37 +0200

    [FIX] remove column rename

--- addons/maintenance/migrations/10.0.1.0/pre-migration.py
+++ addons/maintenance/migrations/10.0.1.0/pre-migration.py
@@ -3,15 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openupgradelib import openupgrade
 
-_column_renames = {
-    'maintenance_equipment': [
-        ('warranty', 'warranty_date'),
-    ],
-    'maintenance_request': [
-        ('technician_user_id', 'user_id'),
-    ],
-}
-
 _model_renames = [
     ('machine.repair', 'maintenance.equipment'),
 ]
@@ -27,4 +18,3 @@ def migrate(env, version):
     if openupgrade.table_exists(cr, 'machine_repair'):
         openupgrade.rename_models(cr, _model_renames)
         openupgrade.rename_tables(cr, _table_renames)
-    openupgrade.rename_columns(cr, _column_renames)
